51156b415763f6244a4d71eda2382c62b8ed7b17
nalquas <nalquas.dev@gmail.com>
2023-02-11 23:24:39 +0100

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,20 @@
       <router-view></router-view>
     </main>
     <footer>
-      <!-- TODO some footer -->
+      <Footer></Footer>
     </footer>
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    NavBar
+    NavBar,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div class="container-lg d-flex d-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <span class="text-muted">
+        &copy; {{ new Date().getFullYear() }} Niklas Freund
+      </span>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li class="ms-3">
+        <a class="text-muted" href="https://www.linkedin.com/in/niklas-freund/">
+          <i class="bi bi-linkedin"></i>
+        </a>
+      </li>
+      <li class="ms-3">
+        <a class="text-muted" href="https://github.com/nalquas">
+          <i class="bi bi-github"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+i {
+  font-size: 24px;
+}
+</style>
