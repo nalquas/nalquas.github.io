0112060e790ce7e192b0044109feb5940acc9532
nalquas <nalquas.dev@gmail.com>
2023-02-11 23:49:15 +0100

    Begin projects page

---
+++ src/components/Hero.vue
@@ -0,0 +1,49 @@
+
+<template>
+  <div class="container px-4 py-5">
+    <div class="row align-items-center g-5 py-5" :class="(this.reverse && 'flex-lg-row-reverse') || 'flex-lg-row'">
+      <div class="col-12 col-lg-6">
+        <img :src="this.imgSrc" class="img-fluid border rounded-3 shadow-lg" alt="Image of the product described in the accompanying text" loading="lazy">
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold lh-1 mb-3"><slot name="title">Title</slot></h1>
+        <p class="lead">
+          <slot></slot>
+        </p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+          <slot name="buttons">
+            <a type="button" class="btn btn-primary btn-lg px-4 me-md-2" :href="this.hrefPrimary"><slot name="buttonPrimary">Primary</slot></a>
+            <a type="button" class="btn btn-outline-secondary btn-lg px-4" :href="this.hrefSecondary"><slot name="buttonSecondary">Secondary</slot></a>
+          </slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: {
+    reverse: {
+      type: Boolean,
+      required: true
+    },
+    imgSrc: {
+      type: String,
+      required: true
+    },
+    hrefPrimary: {
+      type: String,
+      required: false
+    },
+    hrefSecondary: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,13 +1,54 @@
 
 <template>
   <article>
-    <p>
-      TODO Projects
-    </p>
+    <!-- Vogeljagd 2 -->
+    <Hero :reverse="true"
+      :imgSrc="'/images/vogeljagd2-screenshot01.png'"
+      :hrefPrimary="'https://nalquas.itch.io/vogeljagd2'"
+      :hrefSecondary="'https://github.com/nalquas/vogeljagd2'"
+    >
+      <template #title>Vogeljagd 2</template>
+      Vogeljagd 2 is a shoot-em-up where your goal is to hit as many birds as possible in 60 seconds, made in TIC-80.
+      <template #buttonPrimary><i class="bi bi-controller"></i> Play on itch.io</template>
+      <template #buttonSecondary><i class="bi bi-github"></i> GitHub</template>
+    </Hero>
+
+    <!-- Mainfighter -->
+    <Hero class="border-top" :reverse="false"
+      :imgSrc="'/images/mainfighter-gameplay.gif'"
+      :hrefPrimary="'https://nalquas.itch.io/mainfighter'"
+      :hrefSecondary="'https://github.com/nalquas/mainfighter'"
+    >
+      <template #title>Mainfighter</template>
+      Mainfighter is a shoot-em-up with pseudo-3d graphics, made in TIC-80.
+      <template #buttonPrimary><i class="bi bi-controller"></i> Play on itch.io</template>
+      <template #buttonSecondary><i class="bi bi-github"></i> GitHub</template>
+    </Hero>
+
+    <!-- Blobs of Colour -->
+    <Hero class="border-top" :reverse="true"
+      :imgSrc="'/images/blobs-of-colour-screenshot01.png'"
+      :hrefPrimary="'https://nalquas.itch.io/blobs-of-colour'"
+      :hrefSecondary="'https://github.com/nalquas/blobs-of-colour'"
+    >
+      <template #title>Blobs of Colour</template>
+      Blobs of Colour is a game about filling a hall with as much colour as possible in 99 seconds, made with Godot.
+      <template #buttonPrimary><i class="bi bi-controller"></i> Play on itch.io</template>
+      <template #buttonSecondary><i class="bi bi-github"></i> GitHub</template>
+    </Hero>
+
   </article>
 </template>
 
 <script>
+import Hero from './Hero.vue';
+
+export default {
+  name: 'Projects',
+  components: {
+    Hero
+  }
+}
 </script>
 
 <style scoped>
